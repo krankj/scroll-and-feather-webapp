e48f6ca52d51c7dc4305fa51aaac250527a809a2
Sudarshan K J <kjsudi@gmail.com>
2021-01-28 20:13:33 -0800

    Website under development message

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import { Button, Center } from "@chakra-ui/react";
+import { Button, Text, Center } from "@chakra-ui/react";
 import Header from "components/Header";
 function App() {
   return (
     <>
       <Header />
-      <Center>
-        <Button variant="outline">Submit</Button>
+      <Center h="80vh">
+        <Text>
+          This application is currently under development. <br></br>Do come back
+          a while later.
+        </Text>
+        {/* <Button variant="outline">Submit</Button> */}
       </Center>
     </>
   );
